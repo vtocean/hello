6851dbcab44405d59bf861ae66ac4fca5e9afddb
James Truong <vtocean@gmail.com>
2014-10-23 11:11:50 +0000

    import

--- hello.go
+++ hello.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/vtocean/newmath"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	fmt.Printf("Hello, world.  Sqrt(2) = %v\n", newmath.Sqrt(2))
 }
+
